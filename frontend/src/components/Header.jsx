5851817afb51f62347696bacfe1a771d201f417e
arsenez <arsenez@cybercommunity.space>
2026-06-23 23:24:45 +0300

    lint: resolve all linting issues
    Signed-off-by: arsenez <arsenez@cybercommunity.space>

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import styles from './Header.module.css'
 import logo from '../assets/logo.png'
 import Title from './Title';
@@ -20,4 +19,4 @@ function Header() {
   )
 }
 
-export default Header;
+export default Header; 
