78eeb9b1d28d7f327d7956d2ae50a181e5bcc9e1
Deldore <103668969+Deldore@users.noreply.github.com>
2026-07-06 20:58:14 +0300

    Finisged reg and auth page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Landing from './pages/Landing'
 import Registration from './pages/Registration'
+import Authorization from './pages/Authorization'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Landing />}/>
       <Route path='/register' element={<Registration />}/>
+      <Route path='/auth' element={<Authorization />}/>
     </Routes>
   )
 }
--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
 import styles from './Registration.module.css'
-import background from '../assets/backgroundReg.png'
 import secureLogo from '../assets/mingcute_safe-lock-line.svg'
 import { Link } from 'react-router-dom'
 import regLogo from '../assets/mdi_account-box-plus-outline.svg'
+import { useState } from 'react'
+import eyeIcon from '../assets/mynaui_eye.png'
 
 function Registration() {
+    const [showPass, setShowPass] = useState(false);
+    const [showPassConf, setShowPassConf] = useState(false);
+
     return (
         <div className={styles.container}>
             <div className={styles.sidebar}>
@@ -31,15 +34,34 @@ function Registration() {
                     </div>
                     <div className={styles.title}>Create your account</div>
                     <div className={styles.subtitle}>Join GophKeeper and keep your data secure.</div>
-                    <form action="">
-                        <label htmlFor="name">Full name</label>
+                    <form>
+                        <label htmlFor="input">Full name</label>
                         <input className={`${styles.input} ${styles.userInput}`} name='name' placeholder='Enter your full name' />
-                        <label htmlFor="username">Username</label>
+                        <label htmlFor="input">Username</label>
                         <input className={`${styles.input} ${styles.userInput}`} name='username' placeholder='Enter your username' />
-                        <label htmlFor="password">Password</label>
-                        <input className={`${styles.input} ${styles.passInput}`} name='password' placeholder='Create a strong password' />
-                        <label htmlFor="confPass">Confirm password</label>
-                        <input className={`${styles.input} ${styles.passInput}`} name='confPass' placeholder='Confirm your password' />
+                        <label htmlFor="input">Password</label>
+                        <dir className={styles.inputWrapper}>
+                            <input className={`${styles.input} ${styles.passInput}`} name='password' type={showPass ? 'text' : 'password'} placeholder='Create a strong password' />
+                            <button onClick={(e) => {
+                                e.preventDefault();
+                                setShowPass((prev) => { return !prev })
+                         }
+                          }
+                            className={styles.showPassButton}>
+                                <img src={eyeIcon} alt="toggle" />
+                            </button>
+                        </dir>
+                        <label htmlFor="input">Confirm password</label>
+                        <dir className={styles.inputWrapper}>
+                            <input className={`${styles.input} ${styles.passInput}`} name='confPass' type={showPassConf ? 'text' : 'password'} placeholder='Confirm your password' />
+                            <button onClick={(e) => {
+                                e.preventDefault();
+                                setShowPassConf((prev) => { return !prev })    
+                            }}
+                            className={styles.showPassButton}>
+                                <img src={eyeIcon} alt="toggle" />
+                            </button>
+                        </dir>
                         <button className={styles.submitButton}>Create account</button>
                     </form>
                     <div className={styles.hr}>
@@ -48,7 +70,7 @@ function Registration() {
                         <div className={styles.hrLine}></div>
                     </div>
                     <div className={styles.footer}>
-                        Already have account? <Link to='/auth'>Log in</Link>
+                        Already have account? <Link to='/auth' style={{ fontWeight: "500" }}>Log in</Link>
                     </div>
                 </div>
             </div>
